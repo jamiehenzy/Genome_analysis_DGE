0305d7e38c6e7287372def03f01c7d6d5e5f2583
Jamie Henzy <149628307+jamiehenzy@users.noreply.github.com>
2024-05-30 13:56:24 -0400

    Update Reads_to_counts.sh

--- Reads_to_counts.sh
+++ Reads_to_counts.sh
@@ -2,41 +2,23 @@
 
 SECONDS=0
 
-# change working directory
-cd /Users/kr/Desktop/demo/RNASeq_pipeline/
+# change to working directory
 
+# download genome
 
+# download gtf file
 
-# STEP 1: Run fastqc
-fastqc data/demo.fastq -o data/
+# download fastq files
 
+# run fastqc
 
 # run trimmomatic to trim reads with poor quality
-java -jar ~/Desktop/demo/tools/Trimmomatic-0.39/trimmomatic-0.39.jar SE -threads 4 data/demo.fastq data/demo_trimmed.fastq TRAILING:10 -phred33
-echo "Trimmomatic finished running!"
 
-fastqc data/demo_trimmed.fastq -o data/
+# use STAR program to index genome
 
+# align reads using STAR
 
-# STEP 2: Run HISAT2
-# mkdir HISAT2
-# get the genome indices
-# wget https://genome-idx.s3.amazonaws.com/hisat/grch38_genome.tar.gz
-
-
-# run alignment
-hisat2 -q --rna-strandness R -x HISAT2/grch38/genome -U data/demo_trimmed.fastq | samtools sort -o HISAT2/demo_trimmed.bam
-echo "HISAT2 finished running!"
-
-# STEP 3: Run featureCounts - Quantification
-
-
-
-# get gtf
-# wget http://ftp.ensembl.org/pub/release-106/gtf/homo_sapiens/Homo_sapiens.GRCh38.106.gtf.gz
-featureCounts -S 2 -a ../hg38/Homo_sapiens.GRCh38.106.gtf -o quants/demo_featurecounts.txt HISAT2/demo_trimmed.bam
-echo "featureCounts finished running!"
-
+# run featureCounts in subread program (available as env in shared folder)
 
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
